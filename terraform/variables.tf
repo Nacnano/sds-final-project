3f59b527500bf40dec4be958ad605c8086677888
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-11-22 17:08:04 +0700

    feat: add variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,21 @@
 variable "master_ip" {
-  description = "IP address of the K3s master node (existing cluster)"
+  description = "IP address of the K3s master node"
   type        = string
   default     = "192.168.0.103"
 }
+
+variable "worker_ips" {
+  description = "List of IP addresses for Raspberry Pi worker nodes"
+  type        = list(string)
+  default     = ["192.168.0.100", "192.168.0.102", "192.168.0.104", "192.168.0.105"]
+}
+
+variable "ssh_user" {
+  description = "SSH username for worker nodes"
+  type        = string
+  default     = "warissara"
+}
+
 
 variable "registry_port" {
   description = "Port for Docker registry on master node"
