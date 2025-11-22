19b2abc79b1acf8fcb19997225bfceb5f4942f2d
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-11-22 12:17:28 +0700

    feat: remove auth service

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -3,12 +3,10 @@ import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
 import { ShrineModule } from './shrine/shrine.module';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule as SharedAuthModule } from '@app/shared/auth/auth.module';
 import { LocationModule } from './location/location.module';
 
 @Module({
   imports: [
-    SharedAuthModule,
     ShrineModule,
     LocationModule,
     ConfigModule.forRoot({
